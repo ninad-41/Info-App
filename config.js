1304caf3e02212c5a48645fae1535548d376a20f
ninad-41 <76698136+ninad-41@users.noreply.github.com>
2021-05-25 09:50:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 require('@firebase/firestore');
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyDCZBm2uEpv_IbrpxxcL5uTaRepLdAb3n8',
-  authDomain: 'my-coach-app-dd324.firebaseapp.com',
-  projectId: 'my-coach-app-dd324',
-  storageBucket: 'my-coach-app-dd324.appspot.com',
-  messagingSenderId: '674188034221',
-  appId: '1:674188034221:web:6ca2470dd3583a99af4624',
+  apiKey: "AIzaSyDPOeXNUJrNu-jtdXqN-JJNyUrjkjdurEA",
+  authDomain: "info-provider-9b875.firebaseapp.com",
+  projectId: "info-provider-9b875",
+  storageBucket: "info-provider-9b875.appspot.com",
+  messagingSenderId: "527959115894",
+  appId: "1:527959115894:web:bdfea85197389c3d557757"
 };
 //Initialize Firebase
 
